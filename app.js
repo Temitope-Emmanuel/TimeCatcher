932f2d20323b43b240133a4ed8569b9d4161b21b
Temitope Ojo <temitopeojo0@gmail.comgit config --global user.email>
2019-02-09 22:56:18 +0100

    added user sign up

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-var express = require("express"),
-    app     = express(),
-    mongoose = require("mongoose")
+var express                 = require("express"),
+    app                     = express(),
+    bodyParser              = require("body-parser"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    LocalStrategy           = require("passport-local"),
+    myFunction              = require("./models/user"),
+    User                    = myFunction.User
 
 
 mongoose.connect("mongodb://127.0.0.1/time", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended:true}))
 app.set("view engine","ejs")
+
+// PASSPORT CONFIG 
+app.use(require("express-session")({
+    secret:"first tryout",
+    resave:false,
+    saveUninitialized:false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // LANDING PAGE
 app.get("/",function(req,res){
@@ -13,6 +32,20 @@ app.get("/",function(req,res){
 
 app.get("/register",function(req,res){
     res.render("../views/register")
+})
+
+app.post("/register",function(req,res){
+    var newUser = new User({username:req.body.username})
+    User.register(newUser,req.body.password,function(err,user){
+        if(err){
+            console.log(err)
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                console.log(user.username)
+                res.redirect("/register") 
+            })
+        }
+    })
 })
 
 app.listen(3000,function(){
